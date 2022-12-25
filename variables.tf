0608ce2104c5e004d7a79d69fd3497773b9f5628
nimblefox <sk.farooq96406@gmail.com>
2022-12-25 03:32:22 -0600

    adding new resources

--- variables.tf
+++ variables.tf
@@ -9,3 +9,10 @@ variable "region" {
   default     = "us-central1"
   description = "Region where the GCE VM Instance resides. Defaults to the Google provider's region if nothing is specified here. See https://cloud.google.com/compute/docs/regions-zones"
 }
+
+variable "service_account_email" {
+  type        = string
+  default     = "github-tf@silicon-synapse-372206.iam.gserviceaccount.com"
+  description = "Email ID of the service account"
+
+}
